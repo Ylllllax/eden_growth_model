c20c97be0900ef1688efd67933c6252686d7968b
Xuan Chen <xuanchen@live.co.uk>
2017-01-26 23:01:43 +0000

    Did stuff on off-lattice

--- Eden model/Off-lattice ball deposition.py
+++ Eden model/Off-lattice ball deposition.py
@@ -1,9 +1,11 @@
 import pygame
 import random
 import math
+import time
 
 background_colour = (255, 255, 255)
 (width, height) = (800, 800)
+mass_of_air = 0
 
 elasticity = 0    # 0 means balls stick i.e. what percentage of speed is retained on bounce
 
@@ -16,6 +18,30 @@ def addVectors((angle1, length1), (angle2, length2)):
     length = math.hypot(x, y)
 
     return (angle, length)
+
+
+def findParticle(particles, x, y):
+    for p in particles:
+        if math.hypot(p.x-x, p.y-y) <= p.size:
+            return p
+    return None
+
+
+def newParticle(delay):
+    size = random.randint(10, 20)
+    density = random.randint(1, 20)
+    x = random.randint(size, width - size)
+    y = random.randint(size, height - size)
+
+    particle = Particle((x, y), size, density * size ** 2)
+    particle.colour = (200 - density * 10, 200 - density * 10, 255)
+    particle.speed = random.random()
+    particle.angle = random.uniform(0, math.pi * 2)
+
+    my_particles.append(particle)
+
+    if delay is True:
+        time.sleep(1)
 
 
 def collide(p1, p2):
@@ -42,7 +68,7 @@ def collide(p1, p2):
 
 
 class Particle():
-    def __init__(self, (x, y), size):
+    def __init__(self, (x, y), size, mass=1):
         self.x = x
         self.y = y
         self.size = size
@@ -50,15 +76,17 @@ class Particle():
         self.thickness = 0
         self.speed = 0
         self.angle = 0
+        self.mass = mass
+        self.drag = (self.mass/(self.mass + mass_of_air)) ** self.size
 
     def display(self):
         pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), self.size, self.thickness)
 
     def move(self):
-        (self.angle, self.speed) = addVectors((self.angle, self.speed), (0, 0))
+        #(self.angle, self.speed) = addVectors((self.angle, self.speed), gravity)
         self.x += math.sin(self.angle) * self.speed
         self.y -= math.cos(self.angle) * self.speed
-        # self.speed *= drag
+        #self.speed *= self.drag
 
     def bounce(self):
         if self.x > width - self.size:
@@ -82,33 +110,44 @@ class Particle():
             self.speed *= elasticity
 
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Tutorial 5')
+pygame.display.set_caption('Eden deposition off-lattice')
 
 number_of_particles = 10
 my_particles = []
 
 for n in range(number_of_particles):
-    size = random.randint(10, 20)
-    x = random.randint(size, width-size)
-    y = random.randint(size, height-size)
+    newParticle(0)
 
-    particle = Particle((x, y), size)
-    particle.speed = random.random()
-    particle.angle = random.uniform(0, math.pi*2)
-
-    my_particles.append(particle)
-
+selected_particle = None
 running = True
+
 while running:
+
+    newParticle(0)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            (mouseX, mouseY) = pygame.mouse.get_pos()
+            selected_particle = findParticle(my_particles, mouseX, mouseY)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            selected_particle = None
+
+    if selected_particle:
+        (mouseX, mouseY) = pygame.mouse.get_pos()
+        dx = mouseX - selected_particle.x
+        dy = mouseY - selected_particle.y
+        selected_particle.angle = 0.5*math.pi + math.atan2(dy, dx)
+        selected_particle.speed = math.hypot(dx, dy) * 0.1
 
     screen.fill(background_colour)
 
-    for particle in my_particles:
+    for i, particle in enumerate(my_particles):
         particle.move()
         particle.bounce()
+        for particle2 in my_particles[i+1:]:
+            collide(particle, particle2)
         particle.display()
 
     pygame.display.flip()
